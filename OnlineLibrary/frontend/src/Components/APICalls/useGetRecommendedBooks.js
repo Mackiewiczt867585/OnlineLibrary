2d0d99b12378421c8df68d4cdccaa63ea8f53860
mackiewiczt26 <mackiewiczt26@gmail.com>
2023-01-17 05:06:12 +0100

    Main page view fix

--- OnlineLibrary/frontend/src/Components/APICalls/useGetRecommendedBooks.js
+++ OnlineLibrary/frontend/src/Components/APICalls/useGetRecommendedBooks.js
@@ -17,22 +17,27 @@ function useGetRecommendedBooks() {
             })
     }, []);
 
-    return (
-        <>
-            <ol className="list">
-            {data.map(book => (
-                <li className="border" key={book.id}>
+    let recommendations_list = [];
+    for (let i in data){
+        recommendations_list.push(
+            <li className="border" key={data[i].id}>
                             <div className="row">
                                 <div className="col-sm-1">
-                                    <img src={book.image_l} alt="Book cover" className="cover-img"/>
+                                    <img src={data[i].image_l} alt="Book cover" className="cover-img"/>
                                 </div>
                                 <div className="col-sm-5"/>
                                 <div className="col-sm-7">
-                                    <h1><Link to={"/title?t="+String(book.pk)} className="list-item-text">{book.title}</Link></h1> <h2> by {book.author}</h2>
+                                    <h1><Link to={"/title?t="+String(data[i].pk)} className="list-item-text">{data[i].title}</Link></h1> <h2> by {data[i].author}</h2>
                                 </div>
                             </div>
                         </li>
-            ))}
+        );
+    }
+
+    return (
+        <>
+            <ol className="list">
+                {recommendations_list}
             </ol>
         </>
     );
