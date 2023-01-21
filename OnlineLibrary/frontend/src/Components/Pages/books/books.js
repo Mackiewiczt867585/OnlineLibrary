428757996bdc9a74feb0a3c360f6053bbaf9a8ae
mackiewiczt26 <mackiewiczt26@gmail.com>
2023-01-21 19:11:29 +0100

    Added basic pagination.

--- OnlineLibrary/frontend/src/Components/Pages/books/books.js
+++ OnlineLibrary/frontend/src/Components/Pages/books/books.js
@@ -5,6 +5,33 @@ import useGetBooks from "../../APICalls/useGetBooks";
 import {SearchInput, FilterInput} from "../../SearchInput/SearchInput";
 import {Link} from "react-router-dom";
 
+
+function pgup(){
+    const filter_element_id = "offset";
+    let url = new URL(window.location.href);
+    let params = new URLSearchParams(url.search);
+    if (params.get("offset") === null) params.set(filter_element_id, 10);
+    else {
+        params.set(filter_element_id, parseInt(params.get("offset"))+10);
+    }
+    let finalUrl = params.toString() + "&";
+    if (finalUrl.includes("?") !== true) finalUrl = "?" + finalUrl;
+    window.location.replace(finalUrl);
+}
+function pgdn(){
+    const filter_element_id = "offset";
+    let url = new URL(window.location.href);
+    let params = new URLSearchParams(url.search);
+    if (params.get("offset") === "10") params.delete(filter_element_id);
+    else {
+        params.set(filter_element_id, parseInt(params.get("offset"))-10);
+    }
+    let finalUrl = params.toString() + "&";
+    if (finalUrl.includes("?") !== true) finalUrl = "?" + finalUrl;
+    window.location.replace(finalUrl);
+}
+
+
 function BooksPage(){
 
     return (
@@ -24,6 +51,11 @@ function BooksPage(){
                 <div className="col-md-8 card title-border">
                     <h1 className="text-center card-header">Titles</h1>
                     {useGetBooks()}
+                    <div className="col-md-8"></div>
+                    <div className="col-md-4">
+                        <button onClick={pgdn}>Previous Page</button>
+                        <button onClick={pgup}>Next Page</button>
+                    </div>
                 </div>
 
             </div>
